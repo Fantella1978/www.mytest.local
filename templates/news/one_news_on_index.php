e8cf4bae05a6690ba5827282a4c5526dd26937f7
Fantella1978 <fant_pvs@mail.ru>
2018-07-13 23:22:50 +0300

    Signed-off-by: Fantella1978 <fant_pvs@mail.ru>

--- templates/news/one_news_on_index.php
+++ templates/news/one_news_on_index.php
@@ -1,10 +1,10 @@
 <div class="row mb-4 mt-2 animate flipInY news-item-on-index">
 	<div class="col-sm-12 col-md-3 col-lg-2 news-item-img-on-title my-2">
 		<a href="<?php echo $item->url; ?>" title="" target="_top">
-			<img src="//sector.biz.ua<?php echo $item->img_on_title_url; ?>" alt="" title="" class="rounded d-block mx-auto pr-0 pr-md-3 pr-lg-5 pr-xl-4">
+			<img src="//sector.biz.ua<?php echo $item->img_on_title_url; ?>" alt="" title="" class="rounded d-block mx-auto">
 		</a>
 	</div>
-	<div class="col-sm-12 col-md-9 col-lg-10">
+	<div class="col-sm-12 col-md-9 col-lg-10 pl-3">
 		<h4 class="news-item-header">
 			<a href="<?php echo $item->url; ?>" title="" target="_top">
 				<?php echo $item->short_title; ?>
